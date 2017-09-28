8c8edd2b3d3fcef5742ebb4fe2d8efd823244fd3
Mosquito(A) <skykim10908@naver.com>
2017-09-28 10:34:51 +0900

    수정

--- src/employeeTestMain.java
+++ src/employeeTestMain.java
@@ -10,17 +10,28 @@ public class employeeTestMain {
 	public static void main(String[] args) {
 		EmployeeDao dao = EmployeeDao.getInstance();
 		showEmployeeTable(dao);
+		System.out.println("==============================================");
 		
-		Employee e = new Employee(1004);
+		Employee e2 = new Employee(4008, "¼ÛÇý±³", "»ç¿ø", 1003, 1500000, 4, "7½ÃÅð±Ù");
+		dao.insertDepartment(e2);
+		showEmployeeTable(dao);
+		System.out.println("==============================================");
+		
+		Employee e = new Employee(4008);
 		JOptionPane.showMessageDialog(null, dao.selectEmployeeByNo(e));
 		
+		e2.setTitle("´ë¸®");
+		dao.updateDepartment(e2);
+		showEmployeeTable(dao);
+		System.out.println("==============================================");
 		
-		
+		dao.deleteDepartment(e2);
+		showEmployeeTable(dao);
 	}
 
 	private static void showEmployeeTable(EmployeeDao dao) {
 		List<Employee> lists = dao.selectEmployeeByAll();
-		for(Employee e : lists){
+		for (Employee e : lists) {
 			System.out.println(e);
 		}
 	}
